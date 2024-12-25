aa49f87f9282ada314832286383d2af0b2034486
langsjo <104687438+langsjo@users.noreply.github.com>
2024-12-25 22:19:32 +0200

    idk

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -30,10 +30,11 @@
       "langsjo" = import ./home.nix;
     };
   };
-  
+
   environment.systemPackages = with pkgs; [
     nix-prefetch-github
     wget
+    xorg.libXft
     xsel
     wineWowPackages.stable
   ];
@@ -41,7 +42,7 @@
   # programs.firefox.enable = true;
   # services.openssh.enable = true;
 
- #########################################################
+  #########################################################
   nixpkgs.config.allowUnfree = true;
 
   boot.loader.systemd-boot.enable = true;
@@ -51,17 +52,20 @@
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/Helsinki";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fi_FI.UTF-8";
-    LC_IDENTIFICATION = "fi_FI.UTF-8";
-    LC_MEASUREMENT = "fi_FI.UTF-8";
-    LC_MONETARY = "fi_FI.UTF-8";
-    LC_NAME = "fi_FI.UTF-8";
-    LC_NUMERIC = "fi_FI.UTF-8";
-    LC_PAPER = "fi_FI.UTF-8";
-    LC_TELEPHONE = "fi_FI.UTF-8";
-    LC_TIME = "fi_FI.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [ "en_US.UTF-8" "fi_FI.UTF-8" ];
+    extraLocaleSettings = {
+      LC_ADDRESS = "fi_FI.UTF-8";
+      LC_IDENTIFICATION = "fi_FI.UTF-8";
+      LC_MEASUREMENT = "fi_FI.UTF-8";
+      LC_MONETARY = "fi_FI.UTF-8";
+      LC_NAME = "fi_FI.UTF-8";
+      LC_NUMERIC = "fi_FI.UTF-8";
+      LC_PAPER = "fi_FI.UTF-8";
+      LC_TELEPHONE = "fi_FI.UTF-8";
+      LC_TIME = "fi_FI.UTF-8";
+    };
   };
 
   services.xserver.enable = true;
