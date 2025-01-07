a1c5ecb471e176a6bc131c410f641b1ce7668a7b
langsjo <104687438+langsjo@users.noreply.github.com>
2025-01-07 19:10:44 +0200

    Add more warnings for low battery level

--- modules/user/services/dwm-status.nix
+++ modules/user/services/dwm-status.nix
@@ -34,7 +34,7 @@
         icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
 
         enable_notifier = true;
-        notifier_levels = [20 15 10 5 2];
+        notifier_levels = [25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1];
       };
 
       cpu_load = {
