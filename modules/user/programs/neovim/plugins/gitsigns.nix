40e0af038bc6fd3eb65cc6cca258275d08c22bc4
langsjo <104687438+langsjo@users.noreply.github.com>
2025-04-14 17:23:41 +0300

    Added additional git binds to neovim

--- modules/user/programs/neovim/plugins/gitsigns.nix
+++ modules/user/programs/neovim/plugins/gitsigns.nix
@@ -19,9 +19,34 @@
           function()
             require('gitsigns').blame_line()
           end
-        '';
+          '';
         options = {
           desc = "git [b]lame line";
+        };
+      }
+
+      {
+        mode = "v";
+        key = "<leader>hs";
+        action.__raw = ''
+          function()
+            require('gitsigns').stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+          end
+          '';
+        options = {
+          desc = "git [s]tage hunk";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>ht";
+        action.__raw = ''
+          function()
+            require('gitsigns').toggle_current_line_blame()
+          end
+        '';
+        options = {
+          desc = "[T]oggle git show [b]lame line";
         };
       }
     ];
