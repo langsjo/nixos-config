efada127a986dbe7f96f13a38fbee99397c8b584
langsjo <104687438+langsjo@users.noreply.github.com>
2025-06-04 21:00:08 +0300

    feat: add new git aliases

--- modules/user/programs/git.nix
+++ modules/user/programs/git.nix
@@ -6,7 +6,15 @@
     userEmail = "104687438+langsjo@users.noreply.github.com";
 
     aliases = {
+      amend = "commit --amend --no-edit"; # Just add staged files to commit
+      reword = "commit --amend --only"; # Just reword last commit
+
       co = "checkout";
+      s = "status";
+
+      rbi = "rebase -i";
+      rbc = "rebase --continue";
+      rba = "rebase --abort";
     };
 
     extraConfig = {
