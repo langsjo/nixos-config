f5ebde3483e281472ff93ae0a2f3d749fb189493
langsjo <104687438+langsjo@users.noreply.github.com>
2025-11-10 12:58:07 +0200

    feat: add fail2ban

--- modules/system/services/networking.nix
+++ modules/system/services/networking.nix
@@ -4,11 +4,14 @@
 }:
 {
   networking.networkmanager.enable = true;
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+      };
     };
+    fail2ban.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
