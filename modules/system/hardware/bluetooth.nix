781888080c1b5b187052751bebf8b88711f5c7cf
langsjo <104687438+langsjo@users.noreply.github.com>
2025-12-16 14:12:23 +0200

    feat: enable mpris-proxy service for media control

--- modules/system/hardware/bluetooth.nix
+++ modules/system/hardware/bluetooth.nix
@@ -16,5 +16,9 @@ in
   config = lib.mkIf cfg.enable {
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
+
+    systemd.user.services.mpris-proxy = {
+      wantedBy = [ "default.target" ];
+    };
   };
 }
