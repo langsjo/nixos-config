ab9431d51ee128ee3ac960c852dc029e783f91a5
langsjo <104687438+langsjo@users.noreply.github.com>
2025-12-30 21:42:20 +0200

    feat: add nm2nix script package
    also remove options.md, the build was broken for a long time

--- generate-docs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-nix build .#optionDocs
-cat result > OPTIONS.md
-rm result
--- packages/default.nix
+++ packages/default.nix
@@ -5,4 +5,6 @@
 }:
 {
   wrappers = import ./wrappers.nix { inherit inputs pkgs; };
+  rebuild = pkgs.callPackage ./rebuild-script.nix { };
+  nm2nix = pkgs.callPackage ./nm2nix.nix { };
 }
