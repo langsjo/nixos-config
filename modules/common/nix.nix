0d3c8e8368a535c979a5892e5c05332c7e5f21da
langsjo <104687438+langsjo@users.noreply.github.com>
2026-03-24 14:34:34 +0200

    feat: add github access token to nix config

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,5 +1,21 @@
-{ inputs, ... }:
 {
+  config,
+  inputs,
+  ...
+}:
+{
+  sops = {
+    secrets."github-pat" = { };
+    templates."access-tokens" = {
+      owner = config.custom.user.username;
+      group = config.users.users.${config.custom.user.username}.group;
+      path = "${config.custom.user.homeDirectory}/.config/nix/access-tokens.conf";
+      content = ''
+        access-tokens = github.com=${config.sops.placeholder."github-pat"}
+      '';
+    };
+  };
+
   nix = {
     settings = {
       experimental-features = [
@@ -12,6 +28,9 @@
       max-substitution-jobs = 32;
       flake-registry = ""; # Disable the global flake registry
     };
+    extraOptions = ''
+      !include ${config.sops.templates."access-tokens".path}
+    '';
 
     optimise = {
       automatic = true;
@@ -22,7 +41,6 @@
       nixpkgs.flake = inputs.nixpkgs;
       templates.flake = inputs.flake-templates;
     };
-
   };
 
   nixpkgs.config.allowUnfree = true;
