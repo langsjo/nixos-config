11bbf5c94945a08d6e6fb0b2c054ffba41a8db24
langsjo <104687438+langsjo@users.noreply.github.com>
2026-04-08 09:50:39 +0300

    chore: home-manager nixpkgs follows

--- flake.nix
+++ flake.nix
@@ -63,7 +63,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
