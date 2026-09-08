aa3baf57bd9cf1376ab669eb092dead40ba277cb
langsjo <104687438+langsjo@users.noreply.github.com>
2026-09-08 23:39:42 +0300

    fix: remove tailscale auth key

--- modules/programs/tailscale.nix
+++ modules/programs/tailscale.nix
@@ -1,13 +1,7 @@
 {
-  config,
-  ...
-}:
-{
-  sops.secrets."tailscale-auth-key" = { };
   services.tailscale = {
     enable = true;
     openFirewall = true;
-    authKeyFile = config.sops.secrets."tailscale-auth-key".path;
     extraUpFlags = [
       "--login-server=https://headscale.gorilla.gay"
     ];
