4ff7896a83a825d091d41c5b664b5eca28769ea1
langsjo <104687438+langsjo@users.noreply.github.com>
2026-09-08 17:58:09 +0300

    chore: remove unused steam packages

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -8,11 +8,7 @@
   config = lib.mkIf config.custom.gaming.enable {
     programs.steam = {
       enable = true;
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
       extraPackages = with pkgs; [
-        mangohud
         gamescope
         gamemode
       ];
