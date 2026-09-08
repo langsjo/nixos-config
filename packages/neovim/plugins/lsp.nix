ea83ce2107d188d83f2644973cedea21780d09e5
langsjo <104687438+langsjo@users.noreply.github.com>
2026-09-08 16:30:06 +0300

    feat(nvim): don't install cargo and rustc, get from project

--- packages/neovim/plugins/lsp.nix
+++ packages/neovim/plugins/lsp.nix
@@ -52,8 +52,8 @@ in
       };
       rust_analyzer = {
         enable = true;
-        installCargo = true;
-        installRustc = true;
+        installCargo = false;
+        installRustc = false;
         settings.check.command = "clippy";
       };
 
